eda49a832ec14ed55ed008cb24027d0f95273dd2
div-antk <51283468+owaii@users.noreply.github.com>
2020-09-30 21:05:46 +0900

    scssファイルの作成とappファイル修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,32 +1,68 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="{{ app()->getLocale() }}">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>
-    @yield('title')
-  </title>
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-  <!-- Bootstrap core CSS -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Material Design Bootstrap -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Kanban</title>
+
+    <!-- Styles(cloud9の場合、asset=>secure_assetにすること) -->
+    <link href="{{ asset('css/sign_up.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sign_in.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/top.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/button.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/common.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/card_edit.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/card_new.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/card_show.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/list_new.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/list_edit.css') }}" rel="stylesheet">
+
+     <!-- awesome fonts -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+    <!--bootstrap-->
+    <!--CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <!--JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
-
 <body>
-
-  @yield('content')
-
-  <!-- JQuery -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-  <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
+<!-- ログインしている場合のみヘッダを表示する --->
+@auth
+<header class="header">
+  <nav class="nav">
+    <ul class="header_menu">
+      <li class="nav-link">{{Auth::user()->name }}さん</li>
+      <li class="header_menu_title">
+          <a class="nav-link listNew" href="/">kanban</a>
+      </li>
+      <li>
+        <ul class="header_menu_inner">
+          <li>
+              <a class="nav-link listNew" href="{{ route('new') }}">リストを作成</a>　　
+          </li>
+          <li>
+              <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                ログアウト
+              </a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+              </form>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+</header>
+@endauth
+@yield('content')
 </body>
-
 </html>
