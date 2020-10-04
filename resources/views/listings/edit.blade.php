145e53fe21c72c2d9ff6e1d3968bab827bf489a3
div-antk <51283468+owaii@users.noreply.github.com>
2020-10-04 20:34:25 +0900

    editのルーティング、コントローラの編集

---
+++ resources/views/listings/edit.blade.php
@@ -0,0 +1,25 @@
+@extends('app')
+@section('content')
+<div class="panel-body">
+  <!-- バリデーションエラーの場合に表示 --> 
+  @include('common.errors')
+  <form action="{{ route('listings.update', $listing)}}" method="POST" class="form-horizontal">
+    {{csrf_field()}} 
+      <div class="form-group"> 
+        <label for="listing" class="col-sm-3 control-label">リスト名</label> 
+        <div class="col-sm-6"> 
+          <!-- リスト名 --> 
+          <input type="text" name="title" value="{{ old('title', $listing->title) }}" class="form-control"> 
+        </div>
+        <input type="hidden" name="id" value="{{ old('id', $listing->id) }}"> 
+      </div>
+      <div class="form-group"> 
+        <div class="col-sm-offset-3 col-sm-6"> 
+          <button type="submit" class="btn btn-default">
+            <i class="glyphicon glyphicon-saved"></i> 更新
+          </button> 
+        </div>
+      </div>
+    </form>
+</div>
+@endsection
--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -9,7 +9,7 @@
           <h2 class="list_header_title">{{ $listing->title }}</h2>
           <div class="list_header_action">
             <a onclick="return confirm('{{ $listing->title }}を削除して大丈夫ですか？')" href="{{ url('/listingsdelete', $listing->id) }}"><i class="fas fa-trash"></i></a>
-            <a href="{{ url('/listingsedit', $listing->id) }}"><i class="fas fa-pen"></i></a>
+            <a href="{{ route('listings.edit', ['listing' => $listing]) }}"><i class="fas fa-pen"></i></a>
           </div>
         </div>
       </div>
